3c6a3c00cd35df2f9911a6889fbed9bd760b72a1
pswitala <p.switala@pslab.eu>
2020-01-03 20:13:08 +0100

    Added conditionals to validate by R53

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+resource "aws_acm_certificate" "pscloud-acm-cert" {
+  domain_name                 = var.pscloud_domain_name
+  validation_method           = "DNS"
+  subject_alternative_names   = var.pscloud_subject_alternative_names
+}
+
+resource "aws_route53_record" "pscloud-r53-record" {
+  count                       = (var.pscloud_validate_by_r53 == true ? 1 : 0)
+
+  name                        = aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_name
+  type                        = aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_type
+  zone_id                     = var.pscloud_zone_id
+  records                     = [ aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_value ]
+  ttl                         = 60
+
+  depends_on                  = [ aws_acm_certificate.pscloud-acm-cert ]
+}
+
+resource "aws_acm_certificate_validation" "pscloud-cert-validation" {
+  count                       =  (var.pscloud_validate_by_r53 == true ? 1 : 0)
+
+  certificate_arn             = aws_acm_certificate.pscloud-acm-cert.arn
+  validation_record_fqdns     = [ aws_route53_record.pscloud-r53-record[count.index].fqdn ]
+
+  timeouts {
+    create = "120m"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "pscloud_env" {}
+variable "pscloud_company" {}
+
+variable "pscloud_domain_name" {}
+variable "pscloud_zone_id" {}
+variable "pscloud_subject_alternative_names" {}
+
+variable "pscloud_validate_by_r53" {}
