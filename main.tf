44b26209c9e61eefe78274ede5e39229afe97d2b
pswitala <Da39a3ee5G>
2020-08-27 20:45:18 +0200

    Fix updated type of domain_validation_options to list

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "aws_acm_certificate" "pscloud-acm-cert" {
 resource "aws_route53_record" "pscloud-r53-record" {
   count                       = (var.pscloud_validate_by_r53 == true ? 1 : 0)
 
-  name                        = aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_name
-  type                        = aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_type
+  name                        = tolist(aws_acm_certificate.pscloud-acm-cert.domain_validation_options).0.resource_record_name
+  type                        = tolist(aws_acm_certificate.pscloud-acm-cert.domain_validation_options).0.resource_record_type
   zone_id                     = var.pscloud_zone_id
-  records                     = [ aws_acm_certificate.pscloud-acm-cert.domain_validation_options.0.resource_record_value ]
+  records                     = [ tolist(aws_acm_certificate.pscloud-acm-cert.domain_validation_options).0.resource_record_value ]
   ttl                         = 60
 
   depends_on                  = [ aws_acm_certificate.pscloud-acm-cert ]
